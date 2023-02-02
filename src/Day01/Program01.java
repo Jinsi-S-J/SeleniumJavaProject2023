850a48a20e52347f82141150513dd04a27d7a946
Jinsi-S-J <121845984+Jinsi-S-J@users.noreply.github.com>
2023-02-02 09:56:11 +0530

    SelinumDay01

--- src/Day01/Program01.java
+++ src/Day01/Program01.java
@@ -1,9 +1,16 @@
 package Day01;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Program01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		WebDriver driver;
+		driver = new ChromeDriver();
+		driver.get("https://demo.automationtesting.in/Register.html");
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
 
 	}
 
